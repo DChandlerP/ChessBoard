78cd1083d3116fa0659e5ae22a9d96dabc5a106c
DChandlerP <chandler.prince@gmail.com>
2018-03-05 12:38:53 -0600

    Allows Input for X and Y size.

--- ChessBoard.js
+++ ChessBoard.js
@@ -1,7 +1,13 @@
-const size = prompt("Enter the size of the chessboard you require");
-let Output = "";
-for (let x_axis = 0; x_axis < size; x_axis++){
-    for (let y_axis = 0; y_axis < size ; y_axis++) ((x_axis + y_axis) & 1) === 1 ? Output+= "#" : Output += " ";
-    Output += "\n";
+const xsize = parseInt(prompt("Enter the size of the Horizontal Axis that's between 0-9 inclusive "));
+const ysize = parseInt(prompt("Enter the size of the Vertical Axis that's between 0-9 inclusive "));
+let output = "";
+console.log(createBoard(output));
+
+function createBoard(output) {
+    for (let x_axis = 0; x_axis < ysize; x_axis++){
+        for (let y_axis = 0; y_axis < xsize ; y_axis++) ((x_axis + y_axis) & 1) === 1 ? output+= "#" : output += " ";
+        output += "\n";
+    }
+    return output
 }
-console.log(Output);
+
