e6454b82702ead7d5166530da9d3be6008983468
DChandlerP <chandler.prince@gmail.com>
2018-03-09 17:40:05 -0600

    Made way more complex than need be to get myself familiar with other aspects of JS.

--- ChessBoard.js
+++ ChessBoard.js
@@ -1,13 +1,31 @@
-const xsize = parseInt(prompt("Enter the size of the Horizontal Axis that's between 0-9 inclusive "));
-const ysize = parseInt(prompt("Enter the size of the Vertical Axis that's between 0-9 inclusive "));
-let output = "";
-console.log(createBoard(output));
+/*
+I'd seen these problems somewhere before, maybe in the 2nd version, so I already knew the answers. I decided to play
+around with parts of the language I'm less familiar with.
+ */
 
-function createBoard(output) {
-    for (let x_axis = 0; x_axis < ysize; x_axis++){
-        for (let y_axis = 0; y_axis < xsize ; y_axis++) ((x_axis + y_axis) & 1) === 1 ? output+= "#" : output += " ";
+const xsize = parseInt(prompt("Enter the size of the Horizontal Axis that's between 1-9 inclusive "));
+const ysize = parseInt(prompt("Enter the size of the Vertical Axis that's between 1-9 inclusive "));
+
+if(isValid(xsize) & isValid(ysize)) console.log(createBoard(xsize, ysize));
+else window.alert("One of the sizes you entered wasn't between 1-9. Refresh and try again!");
+
+
+function createBoard(xsize, ysize) {
+    let output = "";
+    for (let i = 0; i < ysize; i++){
+        for (let j = 0; j < xsize ; j++) isSumofXandYOdd(i, j)? output+= "#" : output += " ";
         output += "\n";
     }
     return output
 }
+
+
+function isSumofXandYOdd(x_counter, y_counter) {
+    return ((x_counter + y_counter) & 1) === 1;
+}
+
+function isValid(int) {
+    return int < 10 && int > 0;
+}
+
 
